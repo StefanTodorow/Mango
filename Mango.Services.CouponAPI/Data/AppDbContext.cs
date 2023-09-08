74255198190134020180052c6455dbc37d8a8c5b
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-08 17:53:27 +0300

    Seeded Coupon table and automated update-database command

--- Mango.Services.CouponAPI/Data/AppDbContext.cs
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -10,6 +10,27 @@ namespace Mango.Services.CouponAPI.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "10OFF",
+                DiscountAmount = 10,
+                MinAmount = 10
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "20OFF",
+                DiscountAmount = 20,
+                MinAmount = 40
+            });
+        }
+
         public DbSet<Coupon> Coupons { get; set; }
     }
 }
