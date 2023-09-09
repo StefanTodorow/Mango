02199ff35b40c7224b21d7c8e109c120ae066f2f
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-09 15:32:37 +0300

    Added new Static Data(SD) class with enum for Api Type and added RequestDTO in the Web Models

---
+++ Mango.Web/Utillity/SD.cs
@@ -0,0 +1,14 @@
+﻿namespace Mango.Web.Utillity
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+    }
+}
