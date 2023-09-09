a980775ca1edef33d0aced8c4b670e23a84a5dad
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-09 13:57:17 +0300

    Set initial values for ResponseDTO properties in CouponAPI

--- Mango.Services.CouponAPI/Models/DTO/ResponseDTO.cs
+++ Mango.Services.CouponAPI/Models/DTO/ResponseDTO.cs
@@ -3,7 +3,7 @@
     public class ResponseDTO
     {
         public object? Result { get; set; }
-        public bool IsSuccess { get; set; }
-        public string Message { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = string.Empty;
     }
 }
