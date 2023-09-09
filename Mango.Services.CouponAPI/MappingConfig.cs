40447c6f242215b7185d19043e657181b9eb7f89
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-09 14:23:37 +0300

    Created MappingConfig class creating map for Coupon and CouponDTO

---
+++ Mango.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.DTO;
+
+namespace Mango.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(conf =>
+            {
+                conf.CreateMap<Coupon, CouponDTO>();
+                conf.CreateMap<CouponDTO, Coupon>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
