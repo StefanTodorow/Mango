cfde32a7a7a7a638b6ef1777264e09106541adc8
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-09 15:40:34 +0300

    Added BaseService class inheriting IBaseService

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,21 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.DTO;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ResponseDTO> SendAsync(RequestDTO requestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
