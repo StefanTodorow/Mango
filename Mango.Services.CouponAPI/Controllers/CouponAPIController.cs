a33689ee5c5f22fcb0180ba2dbd728bec5201b22
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-09 13:48:07 +0300

    Created the CouponAPIController with 2 get methods

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,48 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db = null;
+
+        public CouponAPIController(AppDbContext db)
+        {
+                _db = db;
+        }
+
+        [HttpGet]   
+        public object Get()
+        {
+            try
+            {
+                return _db.Coupons.ToList();
+            }
+            catch
+            {
+            }
+
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                return _db.Coupons.First(c => c.CouponId == id);
+            }
+            catch
+            {
+            }
+
+            return null;
+        }
+    }
+}
