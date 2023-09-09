d15abcfb8df063dd5d1fc0caf3873c3457d338a0
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-09 15:36:12 +0300

    Added IBaseService interface to the Web in new folder

---
+++ Mango.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.DTO;
+
+namespace Mango.Web.Service.IService
+{
+    public interface IBaseService
+    {
+        Task<ResponseDTO> SendAsync(RequestDTO requestDTO);
+    }
+}
