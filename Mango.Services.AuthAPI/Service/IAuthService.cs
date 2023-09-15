19cb229953ca8df8666b42fb8f5e83d80de6120f
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-15 17:13:48 +0300

    Added IAuthService interface to AuthAPI

---
+++ Mango.Services.AuthAPI/Service/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.AuthAPI.Models.DTO;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public interface IAuthService
+    {
+        Task<UserDTO> Register(RegistrationRequestDTO requestDTO);
+        Task<LoginResponseDTO> Login(LoginRequestDTO requestDTO);
+    }
+}
