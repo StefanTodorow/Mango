81d8f9918d897a25bb15fc2f97f1199ab34dc360
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-16 11:38:50 +0300

    Implemented JwtTokenGenerator class in AuthAPI to generate token

--- Mango.Services.AuthAPI/Service/JwtTokenGenerator.cs
+++ Mango.Services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -1,5 +1,9 @@
 ﻿using Mango.Services.AuthAPI.Models;
 using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Mango.Services.AuthAPI.Service
 {
@@ -13,7 +17,30 @@ namespace Mango.Services.AuthAPI.Service
         }
         public string GenerateToken(ApplicationUser applicationUser)
         {
-            throw new NotImplementedException();
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+
+            var claimList = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Email, applicationUser.Email),
+                new Claim(JwtRegisteredClaimNames.Sid, applicationUser.Id),
+                new Claim(JwtRegisteredClaimNames.Name, applicationUser.UserName)
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Audience = _jwtOptions.Audience,
+                Issuer = _jwtOptions.Issuer,
+                Subject = new ClaimsIdentity(claimList),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
