78f0773e65e53194d8cf7be714d2ac73c37e24fa
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-17 13:34:53 +0300

    Added AuthController to the Web

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+                _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO loginRequestDTO = new();
+            return View(loginRequestDTO);
+        }
+    }
+}
