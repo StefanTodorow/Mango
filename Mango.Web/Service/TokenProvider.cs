42681c29029f340a147597d3084613f502ef4067
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-19 15:46:38 +0300

    Added ITokenProvider interface to the Web to store JWT token in cookies

---
+++ Mango.Web/Service/TokenProvider.cs
@@ -0,0 +1,29 @@
+﻿using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class TokenProvider : ITokenProvider
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public TokenProvider(IHttpContextAccessor httpContextAccessor)
+        {
+                _contextAccessor = httpContextAccessor;
+        }
+
+        public void ClearToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? GetToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
