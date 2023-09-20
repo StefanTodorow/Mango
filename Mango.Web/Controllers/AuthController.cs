599af0bf0ba67e69163c6b562e4357f69109b6cc
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-20 12:52:30 +0300

    Extracted the Role from the JWT of the User when Signing in and added the claim to the indentity

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -119,6 +119,8 @@ namespace Mango.Web.Controllers
 
             identity.AddClaim(new Claim(ClaimTypes.Name,
                 jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+            identity.AddClaim(new Claim(ClaimTypes.Role,
+                jwt.Claims.FirstOrDefault(u => u.Type == "role").Value));
 
             var principal = new ClaimsPrincipal(identity);
 
