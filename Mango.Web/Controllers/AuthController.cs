a52ec5070b9d2353b743e89867940d8eb49f6f6c
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-20 12:33:49 +0300

    Implemented the Logout logic in the Web

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -96,9 +96,12 @@ namespace Mango.Web.Controllers
             return View(obj);
         }
 
-        public IActionResult Logout()
+        public async Task<IActionResult> Logout()
         {
-            return View();
+            await HttpContext.SignOutAsync();
+            _tokenProvider.ClearToken();
+
+            return RedirectToAction("Index", "Home");
         }
 
         private async Task SignInUser(LoginResponseDTO model)
