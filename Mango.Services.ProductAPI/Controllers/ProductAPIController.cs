b8cc1beb143aeec031a322c8bf67c890261241bf
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-27 22:26:36 +0300

    Implemented all CRUD operations on ProductAPI's Controller

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models;
 using Mango.Services.ProductAPI.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,6 +30,100 @@ namespace Mango.Services.ProductAPI.Controllers
             {
                 var objList = _db.Products.ToList();
                 _response.Result = _mapper.Map<IEnumerable<ProductDTO>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDTO Get(int id)
+        {
+            try
+            {
+                var obj = _db.Products.First(c => c.ProductId == id);
+                _response.Result = _mapper.Map<ProductDTO>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("GetByName/{name}")]
+        public ResponseDTO GetByName(string name)
+        {
+            try
+            {
+                var obj = _db.Products.First(c => c.Name.ToLower() == name.ToLower());
+                _response.Result = _mapper.Map<ProductDTO>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDTO Post([FromBody] ProductDTO couponDTO)
+        {
+            try
+            {
+                var obj = _mapper.Map<Product>(couponDTO);
+
+                _db.Products.Add(obj);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDTO Put([FromBody] ProductDTO couponDTO)
+        {
+            try
+            {
+                var obj = _mapper.Map<Product>(couponDTO);
+
+                _db.Products.Update(obj);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ResponseDTO Delete(int id)
+        {
+            try
+            {
+                var obj = _db.Products.First(c => c.ProductId == id);
+
+                _db.Products.Remove(obj);
+                _db.SaveChanges();
             }
             catch (Exception ex)
             {
