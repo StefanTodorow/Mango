c5ce70c2d59af843d8912c4e7703fad5e747456e
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-28 12:41:04 +0300

    Added ProductDTO to the Web and Created IProductService

---
+++ Mango.Web/Service/IService/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface IProductService
+    {
+        Task<ResponseDTO?> GetProductAsync(string name);
+        Task<ResponseDTO?> GetAllProductsAsync();
+        Task<ResponseDTO?> GetProductByIdAsync(int id);
+        Task<ResponseDTO?> CreateProductAsync(ProductDTO productDTO);
+        Task<ResponseDTO?> UpdateProductAsync(ProductDTO productDTO);
+        Task<ResponseDTO?> DeleteProductAsync(int id);
+
+    }
+}
