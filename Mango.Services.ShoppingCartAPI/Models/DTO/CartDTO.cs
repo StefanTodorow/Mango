a892db7cb2dffe303e1969883834295c35d40060
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-29 09:37:10 +0300

    Copied CouponDTO and ResponseDTO to ShoppingCartAPI also added CartDTO containing CartHeaderDTO and IEnumerable of CartDetailsDTO

---
+++ Mango.Services.ShoppingCartAPI/Models/DTO/CartDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.DTO
+{
+    public class CartDTO
+    {
+        public CartHeaderDTO CartHeader { get; set; }
+        public IEnumerable<CartDetailsDTO>? CartDetails { get; set; }
+    }
+}
