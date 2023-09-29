00c98ca6677784f8cb7519c12c8c3583885e5cda
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-29 20:35:29 +0300

    Added ICouponService and implemented it in CouponService in ShoppingCartAPI to get coupon DTO from CouponAPI

---
+++ Mango.Services.ShoppingCartAPI/Service/CouponService.cs
@@ -0,0 +1,29 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.DTO;
+using Mango.Services.ShoppingCartAPI.Service.IService;
+using Newtonsoft.Json;
+
+namespace Mango.Services.ShoppingCartAPI.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CouponService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<CouponDTO> GetCoupon(string couponCode)
+        {
+            var client = _httpClientFactory.CreateClient("Coupon");
+            var response = await client.GetAsync($"/api/coupon/GetByCode/{couponCode}");
+            var apiContent = await response.Content.ReadAsStringAsync();
+            var responseDTO = JsonConvert.DeserializeObject<ResponseDTO>(apiContent);
+
+            if (responseDTO.IsSuccess)
+                return JsonConvert.DeserializeObject<CouponDTO>(Convert.ToString(responseDTO.Result));
+
+            return new CouponDTO();
+        }
+    }
+}
