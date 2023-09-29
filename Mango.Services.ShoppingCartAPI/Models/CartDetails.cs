a677bd12dcc38b523cd7d449bd7aa8fa8082462f
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-29 09:27:11 +0300

    Added 2 Models in ShoppingCartAPI: CartHeader and CartDetails, also copied ProductDTO to ShoppingCartAPI's DTOs

---
+++ Mango.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,17 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.DTO;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; }
+        public int ProductId { get; set; }
+        [NotMapped]
+        public ProductDTO Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
