a21596381a67c63e19905495591504dd9afaff22
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-29 09:46:53 +0300

    Added the basic API configuration for ShoppingCartAPI(DbContext, Extensions, AutoMapper, Authentication, Authorization, appsettings)

---
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.DTO;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(conf =>
+            {
+                conf.CreateMap<CartHeader, CartHeaderDTO>();
+                conf.CreateMap<CartDetails, CartDetailsDTO>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
