151a283ab280891ee9d1f6d86b83345ecdc0a311
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-29 09:33:18 +0300

    Added CartDetailsDTO and CartHeaderDTO to ShoppingCartAPI

---
+++ Mango.Services.ShoppingCartAPI/Models/DTO/CartHeaderDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.DTO
+{
+    public class CartHeaderDTO
+    {
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double CartTotal { get; set; }
+    }
+}
