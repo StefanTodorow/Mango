15b3a522efe4e414d0171b5f41ed5474e4d4b8e1
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-30 16:19:02 +0300

    Added EmailAPI with basic API settings and copied cart related DTOs to EmailAPI

---
+++ Mango.Service.EmailAPI/Models/DTO/CartDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.EmailAPI.Models.DTO
+{
+    public class CartDTO
+    {
+        public CartHeaderDTO CartHeader { get; set; }
+        public IEnumerable<CartDetailsDTO>? CartDetails { get; set; }
+    }
+}
