7931fa5310c01d99252c446ae9b6b94a1886ae33
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-30 14:35:19 +0300

    Added MessageBus interface and implemented it in MessageBus which is using Azure Service Bus

---
+++ Mango.MessageBus/MessageBus.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        private const string connectionString = "Endpoint=sb://mangorestaurantstefantodorov.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=cyVeK+6dw/benKYJEV/XK6DyzJsHQcEc5+ASbOscyLc=";
+        public async Task PublishMessage(object message, string topicOrQueueName)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topicOrQueueName);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+            await sender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
+        }
+    }
+}
