40567764647bfa50e8798565a62026d3b375902c
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-09-30 11:13:49 +0300

    Added backend api authentication http client handler inheriting DelegatingHandler and overriden the SendAsync to pass the Bearer token, Registered the service, added HttpContextAccessor in the web app builder for the service and added HttpMessageHandler for the Product and Coupon HttpClients in the ShoppingAPI's web app builder

--- Mango.Services.ShoppingCartAPI/Program.cs
+++ Mango.Services.ShoppingCartAPI/Program.cs
@@ -4,6 +4,7 @@ using Mango.Services.ShoppingCartAPI.Data;
 using Mango.Services.ShoppingCartAPI.Extensions;
 using Mango.Services.ShoppingCartAPI.Service;
 using Mango.Services.ShoppingCartAPI.Service.IService;
+using Mango.Services.ShoppingCartAPI.Utillity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -24,10 +25,15 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
 
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<BackendApiAuthenticationHttpClientHandler>();
+
 builder.Services.AddHttpClient("Product", client => client.BaseAddress =
-    new Uri(builder.Configuration["ServiceUrls:ProductAPI"]));
+    new Uri(builder.Configuration["ServiceUrls:ProductAPI"]))
+    .AddHttpMessageHandler<BackendApiAuthenticationHttpClientHandler>();
 builder.Services.AddHttpClient("Coupon", client => client.BaseAddress =
-    new Uri(builder.Configuration["ServiceUrls:CouponAPI"]));
+    new Uri(builder.Configuration["ServiceUrls:CouponAPI"]))
+    .AddHttpMessageHandler<BackendApiAuthenticationHttpClientHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Mango.Services.ShoppingCartAPI/Utillity/BackendApiAuthenticationHttpClientHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
+
+namespace Mango.Services.ShoppingCartAPI.Utillity
+{
+    public class BackendApiAuthenticationHttpClientHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public BackendApiAuthenticationHttpClientHandler(IHttpContextAccessor httpContextAccessor)
+        {
+                _contextAccessor = httpContextAccessor;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await _contextAccessor.HttpContext.GetTokenAsync("access_token");
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
