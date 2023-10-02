f237b61f85215e1efcd28ca31bd337b574fd52b8
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-10-02 21:54:52 +0300

    Added StripeRequestDTO to the Web and OrderAPI

---
+++ Mango.Web/Models/StripeRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Web.Models
+{
+    public class StripeRequestDTO
+    {
+        public string StripeSessionUrl { get; set; }
+        public string StripeSessionId { get; set; }
+        public string ApprovedUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public OrderHeaderDTO OrderHeader { get; set; }
+    }
+}
