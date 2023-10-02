24ae053517505146f2a407889d615cbaa06c9d77
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-10-02 16:31:41 +0300

    Created necessary maps in the mapping config in OrderAPI

--- Mango.Services.OrderAPI/MappingConfig.cs
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.DTO;
 
 namespace Mango.Services.OrderAPI
 {
@@ -8,6 +10,16 @@ namespace Mango.Services.OrderAPI
         {
             var mappingConfig = new MapperConfiguration(conf =>
             {
+                conf.CreateMap<OrderHeaderDTO, CartHeaderDTO>()
+                    .ForMember(dest => dest.CartTotal, u => u.MapFrom(src => src.OrderTotal)).ReverseMap();
+
+                conf.CreateMap<CartDetailsDTO, OrderDetailsDTO>()
+                    .ForMember(dest => dest.ProductName, u => u.MapFrom(src => src.Product.Name))
+                    .ForMember(dest => dest.Price, u => u.MapFrom(src => src.Product.Price));
+
+                conf.CreateMap<OrderDetailsDTO, CartDetailsDTO>();
+                conf.CreateMap<OrderHeader, OrderHeaderDTO>().ReverseMap();
+                conf.CreateMap<OrderDetailsDTO, OrderDetails>();
             });
 
             return mappingConfig;
