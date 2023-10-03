6f2918cc27190a47d3bf7305a86bced99347aafd
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-10-03 15:29:24 +0300

    Configured Datatable endpoint for OrderIndex in OrderController in the Web

--- Mango.Web/Controllers/OrderController.cs
+++ Mango.Web/Controllers/OrderController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Mango.Web.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
         public IActionResult OrderIndex()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderHeaderDTO> list;
+            string userId = string.Empty;
+
+            if (!User.IsInRole(SD.RoleAdmin))
+            {
+                userId = User.Claims.Where(cl => cl.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            }
+
+            ResponseDTO response = _orderService.GetAllOrders(userId).GetAwaiter().GetResult();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<IEnumerable<OrderHeaderDTO>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                list = new List<OrderHeaderDTO>();
+            }
+
+            return Json(new { data = list });
         }
     }
 }
