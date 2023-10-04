4cb40a5904b3abff7b2de275e714f6b290ada589
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-10-04 16:23:03 +0300

    Revert "Added Authentication to Ocelot in the Gateway"
    This reverts commit a0363f0d6dc7562ac6f781b1036fcae2025e9141.

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -1,10 +1,8 @@
-using Mango.GatewaySolution.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddAppAuthentication();
 builder.Services.AddOcelot();
 
 var app = builder.Build();
