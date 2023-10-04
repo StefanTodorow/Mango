a0363f0d6dc7562ac6f781b1036fcae2025e9141
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-10-04 14:15:40 +0300

    Added Authentication to Ocelot in the Gateway

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -1,8 +1,10 @@
+using Mango.GatewaySolution.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddAppAuthentication();
 builder.Services.AddOcelot();
 
 var app = builder.Build();
