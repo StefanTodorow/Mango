b9f922f423fd9a73b16997672ed2b93844a60838
Stefan Todorov <stefan_cvetanov_todorov@abv.bg>
2023-10-04 16:23:10 +0300

    Revert "Configured Gateway to use Ocelot"
    This reverts commit 902a384555fe4e28e01f832005c7cde3694fe0d7.

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -1,12 +1,6 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddOcelot();
-
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.UseOcelot();
+
 app.Run();
